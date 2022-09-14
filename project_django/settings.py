c96929bd122abf34deefd815fb1b74245c7e6ef6
adryanhaska <111515167+adryanhaska@users.noreply.github.com>
2022-09-14 23:52:12 +0700

    Update settings.py

--- project_django/settings.py
+++ project_django/settings.py
@@ -132,8 +132,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
