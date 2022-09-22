ea33ce46e1a7e7c74ba9c259821ca19c5cb3a671
adryanhaska <111515167+adryanhaska@users.noreply.github.com>
2022-09-22 10:43:22 +0700

    Update urls.py

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from mywatchlist.views import show_mywatchlist
-from mywatchlist.views import importshow_html 
+from mywatchlist.views import show_html 
 from mywatchlist.views import show_json
 from mywatchlist.views import show_json_by_id
 from mywatchlist.views import show_xml
